d1b6256629f1e2be9da534b6344470fdceba8927
James Backhouse <jwbackhouse@gmail.com>
2020-05-23 10:14:45 +0100

    Removed dummy expense data

--- src/app.js
+++ src/app.js
@@ -13,30 +13,9 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
-
-// Set up store & log / subscribe to changes
+// Set up store
 const store = configureStore();
-// store.subscribe(() => {
-//   const state = store.getState();
-//   const viewVisibleExpenses = viewVisible(state.expenses, state.filters);
-//   console.log(viewVisibleExpenses);
-// });
 
-// Run actions
-const now = moment().valueOf(); // set current Unix time
-store.dispatch(addExpense({ description: 'Football', notes: 'test notes', amount: 6000, createdAt: now }));
-store.dispatch(addExpense({ description: 'Handball', notes: 'test notes', amount: 7000, createdAt: now }));
-store.dispatch(addExpense({ description: 'Netball', notes: 'test notes', amount: 2000, createdAt: now }));
-store.dispatch(addExpense({ description: 'Tennis', notes: 'test notes', amount: 3000, createdAt: 1554249600 }));
-
-// store.dispatch(setTextFilter('ball'));
-// store.dispatch(setTextFilter('tb'));
-
-// Log visible expenses to console
-const state = store.getState();
-const viewVisibleExpenses = viewVisible(state.expenses, state.filters);
-  
-  
 // Render output
 const jsx = (
   <Provider store={ store }>
